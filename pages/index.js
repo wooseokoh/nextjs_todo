f48ac724cd26ce71edbb3bcfd73063927ebdfead
wooseokoh <allsafe1206@gmail.com>
2022-08-30 19:20:20 +0900

    add fontawesome, appBar

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,30 @@
-import { Button } from "@mui/material";
+import { AppBar, Button, Toolbar } from "@mui/material";
 import Head from "next/head";
+import Link from "../src/Link";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>TODO</title>
+        <title>홈 | TODO</title>
         <meta name='description' content='할일관리' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
-      <Button variant='outlined'>버튼</Button>
-
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <AppBar position='fixed'>
+        <Toolbar>
+          <div className='flex-1'></div>
+          <Link href='/' noLinkStyle className='font-bold'>
+            HAPPY NOTE
+          </Link>
+          <div className='flex-1 flex justify-end'>
+            <Link href='/write' noLinkStyle>
+              <i className='fa-solid fa-pen'></i>
+            </Link>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Toolbar />
     </>
   );
 }
