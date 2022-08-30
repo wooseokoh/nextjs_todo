2a0f21603c556774a698407ca7cd45c28ffc82b3
wooseokoh <allsafe1206@gmail.com>
2022-08-30 17:42:33 +0900

    tailwind Normalize setup

--- src/theme.js
+++ src/theme.js
@@ -3,7 +3,15 @@ import { red } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {},
+  typography: {
+    fontFamily: ["GmarketSansMedium"],
+  },
+  palette: {
+    primary: {
+      main: "#ff8686",
+      contrastText: "#ffffff",
+    },
+  },
 });
 
 export default theme;
