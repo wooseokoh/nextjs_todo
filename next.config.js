376be68ef3fea3b16db806fcd4dafcb86649540c
wooseokoh <allsafe1206@gmail.com>
2022-09-04 23:29:18 +0900

    Recoil Code Rf

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
+const intercept = require("intercept-stdout");
+
+// safely ignore recoil warning messages in dev (triggered by HMR)
+function interceptStdout(text) {
+  if (text.includes("Duplicate atom key")) {
+    return "";
+  }
+  return text;
+}
+
+if (process.env.NODE_ENV === "development") {
+  intercept(interceptStdout);
+}
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
